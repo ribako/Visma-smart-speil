67854cff9737f58c01ac98daaf396a32887ff32f
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-11-06 15:25:54 +0100

    Jokes api working

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
 import { api } from "../services/api";
 
 const Joke = () => {
-  return <>HELLO</>;
+  const [joke, setJoke] = useState("");
+  useEffect(() => {
+    handleFetch();
+  }, []);
+  const handleFetch = () => {
+    return api
+      .get("https://api.jokes.one/jod")
+      .then((response) => {
+        console.log(response);
+        setJoke(response.data.contents.jokes[0].joke.text);
+      })
+      .catch(() => "Klarte ikke hente dagens spøk");
+  };
+  return <>{joke}</>;
 };
 
 export default Joke;
