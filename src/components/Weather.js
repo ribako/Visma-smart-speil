200d5cbd787a583dc75f6dd0e28217a7913ccd9f
Kent Erik Banken <kebanken@gmail.com>
2021-11-06 16:14:51 +0100

    La til værikoner

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,23 @@ import {useEffect, useState} from "react";
 function Weather() {
     const [data, setData] = useState()
 
+    const getWeatherByHour = (hour) => {
+        return data?.data?.properties?.timeseries[hour]
+    }
+    
+    const getTemperatureData = (timeserie_element) => {
+        return timeserie_element?.data?.instant?.details
+    }
+
+    const getWeatherIcon = (timeserie_element) => {
+        if (timeserie_element) {
+            return `../icons/${timeserie_element?.data?.next_1_hours?.summary?.symbol_code}.svg`
+        }
+    }
+
     const doCall= () => {
-        api.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=0")
+        api.get("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.92&lon=10.67 ")
             .then(data => {
-                console.log(data);
                 setData(data)
             })
     }
@@ -17,7 +30,10 @@ function Weather() {
     }, [])
 
     return (
-        <>{data?.data?.type}</>
+        <div>
+            <p>Temperatur: {getTemperatureData(getWeatherByHour(0))?.air_temperature}</p>
+            <p>Ikonpath: {getWeatherIcon(getWeatherByHour(0))}</p>
+        </div>
     );
 }
 
