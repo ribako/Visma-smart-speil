ddfcd0c87a4429788eb8368f6cdf5627452007a0
andrerim <andreasrim@gmail.com>
2021-11-06 16:26:50 +0100

    Add reddit jokes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Joke from "./components/Joke";
+import "./App.css";
+import RedditJoke from "./components/RedditJoke";
 
 function App() {
   return (
     <div className="App">
-       <Weather/>
-      <Joke/>
+      <Weather />
+      <Joke />
+      <RedditJoke />
     </div>
   );
 }
---
+++ src/jokes/reddit/index.js
@@ -0,0 +1,4 @@
+import JOKE1 from "./joke1.jpg";
+import JOKE2 from "./joke2.jpg";
+
+export { JOKE1, JOKE2 };
