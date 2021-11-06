94415c762340fda52a0980c556d07695924334d4
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-11-06 16:31:46 +0100

    Fixed import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Joke from "./components/Joke";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
-       <Weather/>
-      <Joke/>
+      <Weather />
+      <Joke />
     </div>
   );
 }
